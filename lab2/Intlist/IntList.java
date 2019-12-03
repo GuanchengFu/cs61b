e830a622ca405017551202304b9cd65821807604
GuanchengFu <sggfu.rick@gmail.com>
2019-12-03 11:35:04 +0000

    Finish lab 2

--- lab2/Intlist/IntList.java
+++ lab2/Intlist/IntList.java
@@ -79,10 +79,14 @@ public class IntList {
      * Returns a list consisting of the elements of A followed by the
      * *  elements of B.  May modify items of A. Don't use 'new'.
      */
-
     public static IntList dcatenate(IntList A, IntList B) {
         //TODO:  fill in method
-        return null;
+        IntList res = A;
+        while (A.rest != null){
+            A = A.rest;
+        }
+        A.rest = B;
+        return res;
     }
 
     /**
@@ -91,7 +95,27 @@ public class IntList {
      */
     public static IntList catenate(IntList A, IntList B) {
         //TODO:  fill in method
-        return null;
+        IntList res = new IntList(A.first, null);
+        IntList ptr = res;
+        A = A.rest;
+        while(A != null){
+            ptr.rest = new IntList(A.first, null);
+            A = A.rest;
+            ptr = ptr.rest;
+        }
+        ptr.rest = B;
+        return res;
+    }
+
+    /**
+     * Return a new copy of the IntList A passed into the function.
+     * Will not modify anything for A.*/
+    public static IntList copy(IntList A){
+        if (A == null){
+            return null;
+        }else{
+            return new IntList(A.first, copy(A.rest));
+        }
     }
 
 
