78dea664d60817c8d3012c83c728112233d2cdca
fgc <sggfu.rick@gmail.com>
2020-07-07 17:06:06 +0800

    Finish hw2

--- hw2/hw2/Percolation.java
+++ hw2/hw2/Percolation.java
@@ -3,5 +3,126 @@ package hw2;
 import edu.princeton.cs.algs4.WeightedQuickUnionUF;
 
 public class Percolation {
+    /**
+     * union(int p, int q)
+     * find(int p): find the root
+     * connected(int p, int q): connect two sites
+     * count(): return the number of components.*/
+    private WeightedQuickUnionUF container;
+    private WeightedQuickUnionUF noBottom;
+    private int openSites = 0;
+    private int length;
+    private int[] sites;
 
+    public Percolation(int N) {
+        if (N <= 0)
+            throw new java.lang.IllegalArgumentException("N must greater than 0.");
+        // Include two virtual sites which are N*N and N*N + 1
+        container = new WeightedQuickUnionUF(N * N + 2);
+        noBottom = new WeightedQuickUnionUF(N * N + 1);
+        length = N;
+        sites = new int[N*N];
+    }
+
+    public void open(int row, int col) {
+        checkValidity(row, col);
+        int index = convertToOne(row, col);
+        if (isOpen(row, col))
+            return;
+        openSites += 1;
+        sites[index] = 1;
+        if (row == 0) {
+            container.union(index, length * length);
+            noBottom.union(index, length * length);
+            if (row == length - 1) {
+                container.union(index, length * length + 1);
+                return;
+            }
+            // length will never be 1 below this line.
+            // Connect with the next line
+            if (isOpen(row + 1, col)) {
+                noBottom.union(index, convertToOne(row + 1, col));
+                container.union(index, convertToOne(row + 1, col));
+            }
+            // Connect with left and right nodes.
+            connectLeftAndRight(row, col);
+        } else if (row == length - 1) {
+            if (isOpen(row - 1, col)) {
+                container.union(index, convertToOne(row - 1, col));
+                noBottom.union(index, convertToOne(row - 1, col));
+            }
+            container.union(index, length * length + 1);
+            connectLeftAndRight(row, col);
+        } else {
+            if (isOpen(row + 1, col)) {
+                container.union(index, convertToOne(row + 1, col));
+                noBottom.union(index, convertToOne(row + 1, col));
+            }
+            if (isOpen(row - 1, col)) {
+                container.union(index, convertToOne(row - 1, col));
+                noBottom.union(index, convertToOne(row - 1, col));
+            }
+            connectLeftAndRight(row, col);
+        }
+    }
+
+    //length will not be 1.
+    private void connectLeftAndRight(int row, int col) {
+        int index = convertToOne(row, col);
+        if (col == 0) {
+            if (isOpen(row, col + 1)) {
+                container.union(index, convertToOne(row, col + 1));
+                noBottom.union(index, convertToOne(row, col + 1));
+            }
+        } else if (col == length - 1) {
+            if (isOpen(row, col - 1)) {
+                container.union(index, convertToOne(row, col - 1));
+                noBottom.union(index, convertToOne(row, col - 1));
+            }
+        } else {
+            if (isOpen(row, col + 1)) {
+                container.union(index, convertToOne(row, col + 1));
+                noBottom.union(index, convertToOne(row, col + 1));
+            }
+            if (isOpen(row, col - 1)) {
+                container.union(index, convertToOne(row, col - 1));
+                noBottom.union(index, convertToOne(row, col - 1));
+            }
+        }
+    }
+
+
+    public boolean isOpen(int row, int col) {
+        checkValidity(row, col);
+        if (sites[convertToOne(row, col)] == 0)
+            return false;
+        return true;
+    }
+
+    public boolean isFull(int row, int col) {
+        checkValidity(row, col);
+        return noBottom.connected(convertToOne(row, col), length * length);
+    }
+
+    private void checkValidity(int row, int col) {
+        if (row < 0 || row > length - 1 || col < 0 || col > length - 1)
+            throw new java.lang.IndexOutOfBoundsException("Invalid arguments used with row and col!");
+    }
+
+    public int numberOfOpenSites() {
+        return openSites;
+    }
+
+    public boolean percolates() {
+        return container.connected(length * length, length * length + 1);
+    }
+
+
+    /**Convert the row and column to an index which can be used in container.*/
+    private int convertToOne(int row, int col) {
+        return row * length + col;
+    }
+
+    public static void main(String[] args) {
+    }
 }
--- hw2/hw2/PercolationStats.java
+++ hw2/hw2/PercolationStats.java
@@ -1,5 +1,46 @@
 package hw2;
 
+import edu.princeton.cs.algs4.StdRandom;
+import edu.princeton.cs.algs4.StdStats;
+
 public class PercolationStats {
 
+
+    private double[] result;
+    private int experimentTimes;
+
+    /** Perform an experiment in a N * N grid.
+     *  Return the percolation threshold.*/
+    private double experiment(Percolation p, int N) {
+        while (!p.percolates()) {
+            int openSite = StdRandom.uniform(0, N * N);
+            if (!p.isOpen(openSite / N, openSite % N))
+                p.open(openSite / N, openSite % N);
+        }
+        return p.numberOfOpenSites() / N * N;
+    }
+
+
+    public PercolationStats(int N, int T, PercolationFactory pf) {
+        if (N <= 0 || T <= 0)
+            throw new java.lang.IllegalArgumentException("N and T should not be smaller than 0.");
+        result = new double[T];
+        for (int i = 0; i < T; i ++) {
+            double temp = experiment(pf.make(N), N);
+            result[i] = temp;
+        }
+        experimentTimes = T;
+    }
+    public double mean() {
+        return StdStats.mean(result);
+    }
+    public double stddev() {
+        return StdStats.stddev(result);
+    }
+    public double confidenceLow() {
+        return mean() - 1.96 * stddev() / Math.sqrt(experimentTimes);
+    }
+    public double confidenceHigh() {
+        return mean() + 1.96 * stddev() / Math.sqrt(experimentTimes);
+    }
 }
